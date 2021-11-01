49dd6cddb7fc8a5cb250ca45c15642c4d468beb5
Dplo1514 <dladlsgur3334@gmail.com>
2021-11-01 19:03:22 +0900

    211101 study

--- src/part_15_3_fileOutPut2.java
+++ src/part_15_3_fileOutPut2.java
@@ -1,22 +1,28 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.FieldPosition;
+import java.util.Scanner;
 
 public class part_15_3_fileOutPut2 {
     public static void main(String args[]) {
         FileWriter fileWriter = null;
 
         try {
-            fileWriter = new FileWriter("src/part_15_3_fileOutput2");
+            fileWriter = new FileWriter("src/part_15_3_fileOutput2",true);
         }catch (IOException e) {
             System.out.println("File 생성에 실패하였습니다");
             System.exit(1);
         }
 
-        try{
-            fileWriter.write("");
-        } catch (){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
+        try{
+            fileWriter.write(input);
+            fileWriter.close();
+        } catch (IOException e){
+            System.out.println("내용입력이 정상샐행되지 않았습니다.");
+            System.exit(2);
         }
     }
 }
