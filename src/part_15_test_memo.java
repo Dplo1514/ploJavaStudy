89b1a3f024f1a2dc27b2c65c19e48ab0e2456698
Dplo1514 <dladlsgur3334@gmail.com>
2021-11-04 21:44:40 +0900

    211104 study

--- src/part_15_test_memo.java
+++ src/part_15_test_memo.java
@@ -1,7 +1,4 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class part_15_test_memo {
@@ -27,22 +24,77 @@ public class part_15_test_memo {
             if (tasknum == 1){
                 System.out.println("메모 파일명을 입력하세요");
                 String filename = scanner.nextLine();
+                FileInputStream fileInput = null;
+
                 try {
-                    FileInputStream fileInput = new FileInputStream(filename);
+                    fileInput = new FileInputStream(filename);
                 }catch (FileNotFoundException e) {
                     System.out.println("파일이 존재하지 않습니다.");
                     continue;
                 }
-                System.out.println("fileName + 내용을 출력합니다.");
+                System.out.println(filename + "내용을 출력합니다.");
+                System.out.println("종료를 원하시면 exit를 입력해주세요");
 
-                Scanner reader = new Scanner(filename);
+                Scanner reader = new Scanner(fileInput);
 
                 while (reader.hasNextLine());{
-                    System.out.println(reader.hasNextLine());
+                    System.out.println(reader.nextLine());
                 }
                 System.out.println("\n");
                 reader.close();
+
+                if (reader.equals("exit")){
+                    System.out.println("파일 읽기를 종료합니다.");
+                    break;
+                }
             }
+
+            //메모 생성 및 쓰기
+            else if(tasknum == 2){
+                System.out.println("저장할 메모 파일명을 입력하세요");
+                String filename = scanner.nextLine();
+
+                FileWriter writer = null;
+
+                try {
+                    writer = new FileWriter(filename);
+                } catch (IOException e) {
+                    System.out.println("파일생성이 정상적으로 되지 않았습니다.");
+                    continue;
+                }
+
+                System.out.println("메모에 저장할 내용을 입력해주세요");
+                System.out.println("입력없이 엔터시 메모가 종료됩니다.");
+
+                while (true){
+                    String input = scanner.nextLine();
+
+                    try{
+                        writer.write(input);
+                        writer.write("\n");
+                    }catch (IOException e){
+                        System.out.println("내용 입력이 정상적으로 처리되지 않았습니다.");
+                        continue;
+                    }
+
+                    if (input.equals("")){
+                        break;
+                    }
+                }
+                try {
+                    writer.close();
+                    System.out.println("메모가 정상적으로 생성되었습니다.");
+                }catch (IOException e) {
+                    System.out.println("프로그램이 정상종료되지 않았습니다.");
+                }
+            }
+
+            else  if(tasknum == 3){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+
 
         }
     }
